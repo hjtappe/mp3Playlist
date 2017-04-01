dda3b8ba80ff34b31c7af57a2b1f29a2a8b243cc
Hans-Juergen Tappe <root@mainzer-stadtmission.de>
2017-04-01 10:21:33 +0200

    Add a script to copy data to the recordings through restricted ssh

---
+++ ssh-copy/copy-script.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -u
+set -e
+
+function usage()
+{
+	echo "Usage: ssh user@host target_filename < source_file" >&2
+	echo "" >&2
+	echo "Cat a file to the Archive, with a given file name." >&2
+	echo "Do not use scp, but a restrivted ssh access." >&2
+	echo "authorized_keys options:" >&2
+	echo "from="source.ip,127.0.0.1",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty,command="~/copy-script.sh" ssh-rsa ..." >&2
+}
+
+
+if [ -z "${SSH_ORIGINAL_COMMAND:-}" ]; then
+	usage
+	exit 1
+fi
+
+FILENAME=`basename "${SSH_ORIGINAL_COMMAND}"`
+SUFFIX="${FILENAME##*.}"
+
+if [ "$SUFFIX" != "mp3" ]; then
+	echo >&2 "Only file names with *.mp3 suffix allowed."
+	exit 1
+fi
+
+echo "cat > ~/Aufnahmen/${FILENAME}"
+cat > ~/Aufnahmen/"${FILENAME}"
+RET=$?
+if [ $RET -eq 0 ]; then
+	# cleanup old recordings
+	find ~/Aufnahmen/ -maxdepth 1 -mindepth 1 -type f -mtime +62 -name "*.mp3" -exec rm -v "{}" \;
+fi
